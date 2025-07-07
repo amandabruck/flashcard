41d5b9db65d54d0d6c363cf2d0fcd330bdd4fec1
amandabruck <125503745+amandabruck@users.noreply.github.com>
2025-07-07 11:35:44 -0300

    fdfj

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    cinema,
+    Qual filme que ganhou 11 Oscars em 2003,
+    Senhor dos Anéis: O Retorno do Rei
+)
+
+criaCartao(
+    cinema,
+    Qual o filme mais longo da história,
+    Logistics, com duração de 857 horas
+)
+
+criaCartao(
+    cinema,
+    Como foi criado os sons do T-Rex em Jurassic Park,
+    Sons de cachorro, elefante e até pinguins misturados
+)
